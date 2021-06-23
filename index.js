63d79f8c611f07b8b5ee841cf834ad64572e9108
kenokenobingo <mail.kewe@gmail.com>
2021-06-23 11:59:03 +0200

    Fixed MQTT

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ require('dotenv').config();
 
 const Web3 = require('web3');
 const fs = require('fs');
-const mqtt = require('mqtt');
+var mqtt = require('mqtt');
 
 let web3 = new Web3(Web3.givenProvider || process.env.INFURA_PATH);
-let client  = mqtt.connect({ port: 1883, host: 'localhost', keepalive: 10000, username: process.env.MQTT_USERNAME, password: process.env.MQTT_PASSWORD});
-
-if(client.connected) {
-  console.log("MQTT connection successfully established!");
-}
+var client = mqtt.connect(process.env.MQTT_HOST, {port: 1883, username: process.env.MQTT_USERNAME, password: process.env.MQTT_PASSWORD});
 
 var abi = fs.readFileSync('./WaterContract.json', {encoding:'utf8', flag:'r'});
 abi = JSON.parse(abi);
@@ -21,8 +17,7 @@ waterInstance = new web3.eth.Contract(abi, address);
 console.log(waterInstance.methods);
 waterInstance.methods.triggerWatering(20, process.env.DAO_REGISTRY).call({from: '0x9fF75B8e1D6A783c2De4535E46d986dbeB09CC31'})
 .then(function(receipt){
-  console.log(receipt)
-  client.on('connect', function () {
-    client.publish('pumpe', 'GO');
-  });
+  console.log(receipt);
+  client.publish('pumpe', 'GO');
+  client.end();
 });
